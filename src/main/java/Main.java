be95ec28f67576213d01e7f07a104cc534636fa1
Marek Lukoszek <m.lukoszek@wide-open.pl>
2021-05-22 11:19:55 +0200

    3 sout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i<3; i++){
+        for (int i = 0; i < 3; i++) {
             System.out.println("Hej");
             System.out.println("Dwa");
             System.out.println("Albo trzy");
